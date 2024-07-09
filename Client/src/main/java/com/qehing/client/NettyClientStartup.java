e0970f77c5ed28ffe1ef883a3ed0218168820eb6
QingheLi <2980267045@qq.com>
2024-07-09 11:42:44 +0800

    test merge dev

--- Client/src/main/java/com/qehing/client/NettyClientStartup.java
+++ Client/src/main/java/com/qehing/client/NettyClientStartup.java
@@ -4,5 +4,6 @@ public class NettyClientStartup {
     public static void main(String[] args) {
 //        new Thread(() -> new NettyRemotingClient().start()).start();
         new NettyRemotingClient().start();
+        System.out.println("test merger dev");
     }
 }
